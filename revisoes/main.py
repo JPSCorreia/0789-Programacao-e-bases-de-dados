4d3ab15fa215ac6c5a67b699bcedc4bf8cec9a45
João Correia <jpscorreia.dev@gmail.com>
2024-01-30 16:43:57 +0000

    1

--- revisoes/main.py
+++ revisoes/main.py
@@ -1,9 +1,19 @@
 # criar variaveis e print
 
-
+a = 5
+b = 2
 print("This line will be printed.")
 
 
-# operação com variaveis.
 
+# operação com variaveis.
+print(a + b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(b // a)
+print(a % b)
+
+print(a ** b)
+print('--'*20)
 
